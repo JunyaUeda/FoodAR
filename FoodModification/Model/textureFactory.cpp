028d63aacbc1fe752d90bfb8b94fd4e82651da4b
Uejun <junya.ueda.iki@gmail.com>
2014-12-08 04:37:59 +0900

    textureFactoryの不要なqDebugを削除

--- FoodModification/Model/textureFactory.cpp
+++ FoodModification/Model/textureFactory.cpp
@@ -26,19 +26,11 @@ void TextureFactory::create(const Region& region, Mat& aTexture) {
     // warpAffine(_src, aTexture, affine_matrix, aTexture.size() , INTER_LINEAR, BORDER_CONSTANT, Scalar::all(0) );
     
     if(region.rotatedRect().size.width > region.rotatedRect().size.height) {
-        qDebug() << "012 width";
-        qDebug() << "vertices[0]" << "x" << vertices[0].x << "    y" << vertices[0].y;
-        qDebug() << "vertices[1]" << "x" << vertices[1].x << "    y" << vertices[1].y;
-        qDebug() << "vertices[2]" << "x" << vertices[2].x << "    y" << vertices[2].y;
         Point2f src_pt[] = { Point2f(0, 0), Point2f(0, _resizedSrc.size().height), Point2f(_resizedSrc.size().width, _resizedSrc.size().height) };
         Point2f dst_pt[] = { vertices[0], vertices[1], vertices[2] };
         Mat affine_matrix = getAffineTransform(src_pt, dst_pt);
         warpAffine(_resizedSrc, aTexture, affine_matrix, aTexture.size() , INTER_LINEAR, BORDER_CONSTANT, Scalar::all(0) );
     } else {
-        qDebug() << "301 height";
-        qDebug() << "vertices[0]" << "x" << vertices[0].x << "    y" << vertices[0].y;
-        qDebug() << "vertices[1]" << "x" << vertices[1].x << "    y" << vertices[1].y;
-        qDebug() << "vertices[2]" << "x" << vertices[2].x << "    y" << vertices[2].y;
         Point2f src_pt[] = { Point2f(0, 0), Point2f(0, _resizedSrc.size().height), Point2f(_resizedSrc.size().width, _resizedSrc.size().height) };
         Point2f dst_pt[] = { vertices[1], vertices[2], vertices[3] };
         Mat affine_matrix = getAffineTransform(src_pt, dst_pt);
