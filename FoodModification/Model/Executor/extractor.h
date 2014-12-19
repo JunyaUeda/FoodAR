41ef59c41f157ae078b3373d00708ae4f0bddc20
Uejun <junya.ueda.iki@gmail.com>
2014-12-19 12:27:06 +0900

    move isInROI

--- FoodModification/Model/Executor/extractor.h
+++ FoodModification/Model/Executor/extractor.h
@@ -34,38 +34,20 @@ private:
     Extractor();
     Extractor(const Extractor&);
 
-    bool isInROI(Point p, Point2f roi[]) {
-        double pro[4];
-        for(int i=0; i<4; ++i) {
-            pro[i] = computeProduct(p, roi[i], roi[(i+1)%4]);
-        }
-        if(pro[0]*pro[2]<0 && pro[1]*pro[3]<0) {
-            return true;
-        }
-        return false;
-    }
-
-    double computeProduct(Point p, Point2f a, Point2f b) {
-        double k = (a.y-b.y)/(a.x-b.x);
-        double j = a.y -k*a.x;
-        return k*p.x -p.y + j;
-    }
-
-
 /*property*/
 private:
 	ExtractService  _extractService;
     EdgeService _edgeService;
     ContourService _contourService;
-	FeatureReference& _featureReference = FeatureReference::getInstance();
-    EdgeFactory& _edgeFactory = EdgeFactory::getInstance();
-    ExtractionManager& _extractionManager = ExtractionManager::getInstance();
-    RegionManager& _regionManager = RegionManager::getInstance();
-    EdgeManager& _edgeManager = EdgeManager::getInstance();
-    ContourState& _contourExisted = ContourExisted::getInstance();
-	ContourState& _contourNonExisted = ContourNonExisted::getInstance();
-    ContourCountState& _oneContour = OneContour::getInstance();
-    ContourCountState& _multiContour = MultiContour::getInstance();
+	FeatureReference& _featureReference    = FeatureReference::getInstance();
+    EdgeFactory& _edgeFactory              = EdgeFactory::getInstance();
+    ExtractionManager& _extractionManager  = ExtractionManager::getInstance();
+    RegionManager& _regionManager          = RegionManager::getInstance();
+    EdgeManager& _edgeManager              = EdgeManager::getInstance();
+    ContourState& _contourExisted          = ContourExisted::getInstance();
+	ContourState& _contourNonExisted       = ContourNonExisted::getInstance();
+    ContourCountState& _oneContour         = OneContour::getInstance();
+    ContourCountState& _multiContour       = MultiContour::getInstance();
    
 
 };
